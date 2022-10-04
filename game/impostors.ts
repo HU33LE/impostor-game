b54ea6ad1c4190460c978a93e983c32720d14004
Daniel Marcano <marcano.daniel98@gmail.com>
2022-10-03 21:50:56 -0500

    Random impostor selection (#1)
        * Player model
        * Randon Impostor Selection algorithm
        * Unit tests coverage

---
+++ game/impostors.ts
@@ -0,0 +1,73 @@
+import Player from "../players/player.model"
+
+export function chooseImpostor(players: Player[], totalImpostors : number = 1) : Player[] {
+    if (players.length == 0) {
+        return [];
+    }
+
+    let sortedPlayers = [...players];
+    let impostors : Player[] = [];
+    let previousValue = sortedPlayers[0].impostorOportunities;
+    let equalChances = true;
+
+    totalImpostors = Math.min(players.length, totalImpostors);
+
+    sortedPlayers = sortedPlayers.sort((a, b) : number => {
+        return b.impostorOportunities - a.impostorOportunities;
+    });
+
+    let totalChances = sortedPlayers.reduce((total, player) : number => {
+        if (equalChances && player.impostorOportunities != previousValue) {
+            equalChances = false;
+        }
+
+        return total + player.impostorOportunities;
+    }, 0);
+
+    if (equalChances) {
+        return chooseEqualChances(sortedPlayers, totalImpostors);
+    }
+
+    for(let i = 0; i < totalImpostors; i++) {
+        const chance = getRandomNumber(totalChances);
+        const impostor = getImpostorForChance(sortedPlayers, chance);
+
+        totalChances -= impostor!.impostorOportunities;
+        
+        impostors.push(impostor!);
+    }
+    
+    return impostors;
+}
+
+function chooseEqualChances(players : Player[], totalImpostors : number = 1) : Player[] {
+    let impostors : Player[] = [];
+
+    for(let i = 0; i < totalImpostors; i++) {
+        const totalChances = players.length;
+        const nextImpostor = getRandomNumber(totalChances);
+        impostors.push(players[nextImpostor]);
+
+        players.splice(nextImpostor, 1);
+    }
+
+    return impostors;
+}
+
+function getImpostorForChance(sortedPlayers : Player[], chosenChance : number) : Player | undefined {
+    for(let index in sortedPlayers) {
+        let player = sortedPlayers[index];
+        if (chosenChance < player.impostorOportunities) {
+            sortedPlayers.splice(index as unknown as number, 1);
+            return player;
+        }
+
+        chosenChance -= player.impostorOportunities;
+    }
+
+    return undefined;
+}
+
+function getRandomNumber(max : number) : number {
+    return Math.floor(Math.random() * max);
+}
